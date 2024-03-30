61031675883181a7d0c7ce1eb243d2a5173181f3
Chernikov23 <118567820+Chernikov23@users.noreply.github.com>
2024-03-30 13:44:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,46 +12,27 @@ def send_welcome(message):
     bot.reply_to(message, "Привет! Пришли мне ссылку на видео YouTube, и я помогу тебе скачать его аудио или видео.")
 
 @bot.message_handler(func=lambda message: True)
-def echo_all(message):
+def handle_message(message):
     chat_id = message.chat.id
     if message.text.startswith('http'):
-        # Сохраняем ссылку и предлагаем пользователю выбор
-        user_choice[chat_id] = {'url': message.text}
-        markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
-        markup.add('Аудио', 'Видео')
-        msg = bot.reply_to(message, "Хочешь скачать аудио или видео?", reply_markup=markup)
-        bot.register_next_step_handler(msg, process_choice)
+        url = message.text
+        send_download_link(chat_id, url)
     else:
         bot.reply_to(message, "Пожалуйста, отправь мне ссылку на YouTube.")
 
-def process_choice(message):
-    chat_id = message.chat.id
-    choice = message.text
-    user_choice[chat_id]['choice'] = choice
-
-    if choice == 'Аудио':
-        download_and_send_file(chat_id, audio=True)
-    elif choice == 'Видео':
-        download_and_send_file(chat_id, audio=False)
-    else:
-        bot.reply_to(message, "Пожалуйста, выбери Аудио или Видео.")
-
-def download_and_send_file(chat_id, audio=True):
-    url = user_choice[chat_id]['url']
+def send_download_link(chat_id, video_url):
+    # Используем yt-dlp для получения прямой ссылки на скачивание
     ydl_opts = {
-        'format': 'bestaudio' if audio else 'best',
-        'outtmpl': '/tmp/%(id)s.%(ext)s',
+        'format': 'bestaudio/best',
+        'quiet': True,
+        'no_warnings': True,
     }
-    
     with YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(url, download=True)
-        filepath = ydl.prepare_filename(info_dict)
-        # Отправляем файл
-        if audio:
-            bot.send_audio(chat_id, audio=open(filepath, 'rb'))
-            print('Скачивание аудио завершено')
+        info_dict = ydl.extract_info(video_url, download=False)
+        if 'url' in info_dict:
+            download_url = info_dict['url']
+            bot.send_message(chat_id, f"Ссылка для скачивания: {download_url}")
         else:
-            bot.send_video(chat_id, video=open(filepath, 'rb'))
-            print('Скачивание видео завершено')
+            bot.send_message(chat_id, "Не удалось получить ссылку для скачивания.")
 
 bot.infinity_polling()
